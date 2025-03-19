8d39072be17a48e8a3e73ee1e9e00a90ecd8d765
ssb00er <sagar.bisht376@gmail.com>
2025-03-20 04:25:57 +0530

    modified

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_messgae_detail(error, error_detail:sys):
     _, _, exc_tb= error_detail.exc_info()
@@ -9,6 +10,8 @@ def error_messgae_detail(error, error_detail:sys):
     )
 
     return error_message
+
+
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
